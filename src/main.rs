7bd84abf211e92600534628c7271d9b5717ff9ba
hello.hacking.icu <hello.hacking.icu@gmail.com>
2025-04-28 02:13:03 +0000

    Add pointer.md

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-struct User;
+struct Node {
+    value: i32,
+    next: Option<Box<Node>>,
+}
 
-fn song(_user: &User) {}
+struct List {
+    head: Option<Box<Node>>,
+}
 
-fn dancing(_user: &User) {}
+impl List {
+    fn new() -> Self {
+        List { head: None }
+    }
+
+    fn push(&mut self, value: i32) {
+        let node = Node {
+            value,
+            next: self.head.take(), // take 是将原来的 head 置空并取出旧值
+        };
+        self.head = Some(Box::new(node));
+    }
+}
 
 fn main() {
-    let user = User{};
-    song(&user);   // 所有权已经转移
-    dancing(&user);  // 编译错误
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
 }
