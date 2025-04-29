9b01de60cac1ec33e0bde044b10239f7bd98e9f5
hello.hacking.icu <hello.hacking.icu@gmail.com>
2025-04-29 09:20:22 +0000

    add lifetimes.md

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
-struct Node {
-    value: i32,
-    next: Option<Box<Node>>,
+trait Animal {}
+
+struct Dog;
+impl Animal for Dog {}
+
+// 这个函数可以处理任何实现了 Animal trait 的类型的引用
+fn feed_animal(_: &dyn Animal) {
+    println!("Feeding an animal");
 }
 
-struct List {
-    head: Option<Box<Node>>,
+// 这个函数只接受 Dog 类型的引用
+fn feed_dog(d: &Dog) {
+    println!("Feeding a dog");
+    feed_animal(d);
 }
 
-impl List {
-    fn new() -> Self {
-        List { head: None }
-    }
+// 定义一个函数指针类型，它接受 &dyn Animal
+type AnimalFeeder = fn(&dyn Animal);
 
-    fn push(&mut self, value: i32) {
-        let node = Node {
-            value,
-            next: self.head.take(), // take 是将原来的 head 置空并取出旧值
-        };
-        self.head = Some(Box::new(node));
-    }
-}
+//定义一个函数指针类型，它接受 &Dog
+type DogFeeder = fn(&Dog);
 
 fn main() {
-    let mut list = List::new();
-    list.push(1);
-    list.push(2);
+    // 一个 AnimalFeeder 类型的变量
+    let animal_feeder: AnimalFeeder = feed_animal;
+
+    // 一个 DogFeeder 类型的变量
+    let dog_feeder: DogFeeder = feed_dog;
+
+    // 下面这行是合法的！
+    let animal_feeder2: AnimalFeeder = feed_dog;
+
+    // 下面这行是不合法的！编译不通过！
+    // let dog_feeder2: DogFeeder = feed_animal;
+
+    //现在，`animal_feeder2` 可以处理 `&dyn Animal` 也可以处理 `&Dog`
+    //虽然其定义的类型为 AnimalFeeder，但是其指向的函数指针 feed_dog, 可以处理 &Dog类型的入参。
+    let dog = Dog;
+    animal_feeder2(&dog);
+    let animal: &dyn Animal = &dog;
+    animal_feeder2(animal);
+
+    //正常调用
+    animal_feeder(animal);
+    dog_feeder(&dog);
 }
